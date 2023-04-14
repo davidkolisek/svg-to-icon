f47bb936d50119dd4c37d0203121468bc72c18c5
dkolisek <david.kolisek@interway.sk>
2023-04-14 22:54:24 +0200

    refactor

--- sass-watch.sh
+++ sass-watch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ### sem vlozit cesty k scss:css suborom
-watch="main.scss:main.css template.scss:template.css"
+watch="demo/main.scss:demo/main.css demo/template.scss:demo/template.css"
 echo "Watcher for: main.scss && template.scss"
 
 sass --watch $watch --style compressed
