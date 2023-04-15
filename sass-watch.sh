4397794e0ec5e03188ce1065547fc7bb72bf0224
dkolisek <david.kolisek@interway.sk>
2023-04-15 07:40:30 +0200

    Cleaning

--- sass-watch.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-### sem vlozit cesty k scss:css suborom
-watch="demo/main.scss:demo/main.css demo/template.scss:demo/template.css"
-echo "Watcher for: main.scss && template.scss"
-
-sass --watch $watch
